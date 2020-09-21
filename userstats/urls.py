38b514772c44d0b0c5719be24e8a54123d69c6db
ayush.acharya96 <ayush.acharya96@gmail.com>
2020-09-21 23:33:29 +0545

    Feat: User income source stat added

--- userstats/urls.py
+++ userstats/urls.py
@@ -1,7 +1,8 @@
-from .views import ExpenseSummaryStats
+from .views import ExpenseSummaryStats, IncomeSourcesSummaryStats
 from django.urls import path
 
 urlpatterns = [
     path('expense-category-data', ExpenseSummaryStats.as_view(), name='expense-category-summary'),
-    
+    path('income-source-data', IncomeSourcesSummaryStats.as_view(), name='income-source-summary'),
+
 ]
